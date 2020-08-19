125625e4a9590d89b5e8aacb6f4baab99686be53
Joshua Israel <joshisrael93@gmail.com>
2020-08-19 15:20:06 -0400

    videos sorted by score

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Video from './Video';
-import Axios from 'axios'
+import Axios from 'axios';
 
 function Home() {
-
-	let [videos, setVideos] = useState([])
+	let [videos, setVideos] = useState([]);
 
 	useEffect(() => {
-		Axios.get('https://umusicvid.herokuapp.com/api/videos').then(
-			(res) => {
-				setVideos(
-					res.data
-				);
-			}
-		);
+		Axios.get('https://umusicvid.herokuapp.com/api/videos').then((res) => {
+			setVideos(
+				res.data.sort(function (a, b) {
+					return b.score - a.score;
+				})
+			);
+		});
 	}, []);
-		
-	let display = videos.map(video => {
-	return (
-		<div>
-			<Video
-								url={video.url}
-								title={video.title}
-								artist={video.artist}
-								genre={video.genre}
-								votes={video.votes}
-								id={video._id}
-								key={video._id}
-							/>
-		</div>
-	);
-	})
 
-	return (
-		<div>
-			{display}
-		</div>
-	)
+	let display = videos.map((video) => {
+		return (
+			<div>
+				<Video
+					url={video.url}
+					title={video.title}
+					artist={video.artist}
+					genre={video.genre}
+					votes={video.votes}
+					id={video._id}
+					key={video._id}
+				/>
+				<h3>Score: {video.score}</h3>
+			</div>
+		);
+	});
+
+	return <div>{display}</div>;
 }
 
 export default Home;
