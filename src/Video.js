e0332a511b6c9630083af0cffc44b88b4dcfce6f
Noah-Jarzab <njarzab721@gmail.com>
2020-08-20 15:43:51 -0600

    Removed some styling we weren't using

--- src/Video.js
+++ src/Video.js
@@ -3,22 +3,17 @@ import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
 
 function Video(props) {
-	const navStyle = {
-		color: 'white',
-	};
 
 	return (
 		<div>
 			{/* <hr></hr> */}
 			<br></br>
 	<h3 className='video-title'>{props.index + 1}. {props.title}</h3>
-			<Link style={navStyle} to='/Largevideos'>
 				<container>
 					<div className='video'>
-						<ReactPlayer controls width='45vw' height='45vh' url={props.url} />
+						<ReactPlayer controls width='45vw' height='30vh' url={props.url} />
 					</div>
 				</container>
-			</Link>
 			<div>
 				<Link to={`/detail/${props.title}`}>
 					<button>Vote</button>
