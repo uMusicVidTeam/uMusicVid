84092f2f6a4906f8aa52b6fd02693e6d66952ec3
Noah-Jarzab <njarzab721@gmail.com>
2020-08-21 02:25:16 -0600

    Attempted to add an icon button... but failed

--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import { Link } from 'react-router-dom';
-// import { FontAwesomeIcon } from '@fortawesome/react-dontawesome';
-
 
 function Video(props) {
 
